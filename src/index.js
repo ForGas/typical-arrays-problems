672ae47a323a82e57dc3c465d511ba0dd0d33a32
Amirbek Akhmetov <lieber_kz@mail.ru>
2020-09-29 14:17:36 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ((array === undefined) ||  (array === null) || (array[0] === undefined)) {
+        return 0;
+    } else {
+        let result = array[0];
+        for(let i = 0; i < array.length; i++) {
+        if (result > array[i]) {
+            result = array[i];
+        }
+    }
+    return result;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if ((array === undefined) ||  (array === null) || (array[0] === undefined))
+      return 0;
+  
+    return array.reduce(function (current, next) {
+        return ( current > next ? current : next );
+    });
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((array === undefined) ||  (array === null) || (array[0] === undefined)) {
+        return 0;
+    } else {
+        let result = 0;
+        let count = 0;
+        for(let i = 0; i < array.length; i++) {
+        result = result + array[i];
+        count++;
+    }
+        return result/count;
+    }
 }
